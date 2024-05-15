03d2f2d7610b01b7174db5fb32cb386e35dd5987
Mahdi <mahdisemsar404@gmail.com>
2024-05-16 01:10:03 +0330

    new class Listener implemented

---
+++ music-player/src/model/Listener.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+abstract public class Listener extends UserAccount {   // abstracting is by my own decision
+    private double credit;
+    private final ArrayList<Playlist> playlists;
+    private final Map<Audio, Integer> audioPlayNum;
+    private Date subExpirationDate;
+    private final ArrayList<Genre> favoriteGenres;
+
+    public Listener(String username, String password, String fullName, String email, String phoneNumber, Date dateOfBirth) {
+        super(username, password, fullName, email, phoneNumber, dateOfBirth);
+        credit = 0.0;
+        playlists = new ArrayList<>();
+        audioPlayNum = new HashMap<>();
+        favoriteGenres = new ArrayList<>();
+        subExpirationDate = null;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    public Map<Audio, Integer> getAudioPlayNum() {
+        return audioPlayNum;
+    }
+
+    public Date getSubscriptionExpirationDate() {
+        return subExpirationDate;
+    }
+
+    public void setSubscriptionExpirationDate(Date subExpirationDate) {
+        this.subExpirationDate = subExpirationDate;
+    }
+
+    public ArrayList<Genre> getFavoriteGenres() {
+        return favoriteGenres;
+    }
+}
