0cbe53113987d1fc066a78b058e0f3820f8e95b6
Mahdi <mahdisemsar404@gmail.com>
2024-05-15 23:46:53 +0330

    Two classes UserAccount and Admin implemented

---
+++ music-player/src/model/UserAccount.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Date;
+
+abstract public class UserAccount {
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private Date dateOfBirth;
+
+    public UserAccount(String username, String password, String fullName, String email, String phoneNumber, Date dateOfBirth) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
